f59b9db15b5e40e39b930f4279381e85c82be12d
SixArm <sixarm@sixarm.com>
2013-08-18 23:09:16 -0700

    Update to use __dir__

--- test/sixarm_ruby_ramp_test/io_test.rb
+++ test/sixarm_ruby_ramp_test/io_test.rb
@@ -2,19 +2,17 @@
 require 'minitest/autorun'
 require 'sixarm_ruby_ramp'
 
-class IOTest < Test::Unit::TestCase
-
- MYDIR=File.join('test','sixarm_ruby_ramp')
+class IOTest < Minitest::Test
 
  def test_load_dir_files
-   dirpath=File.join(MYDIR,'io_test*')
-   expect=[File.join(MYDIR,'io_test.rb'),File.join(MYDIR,'io_test.txt')]
+   dirpath=File.join(__dir__,'io_test*')
+   expect=[File.join(__dir__,'io_test.rb'),File.join(__dir__,'io_test.txt')]
    actual=Dir[dirpath].sort
    assert_equal(expect,actual,"Dir[#{dirpath}] expects test data files")
  end
 
  def test_readrow
-   filename=Dir[File.join(MYDIR,'io_test.txt')].first
+   filename=Dir[File.join(__dir__,'io_test.txt')].first
    f=File.open(filename)
    assert_equal(["A1","B1","C1"],f.readrow(:row=>"=",:col=>"-"))
    assert_equal(["A2","B2","C2"],f.readrow(:row=>"=",:col=>"-"))
@@ -22,7 +20,7 @@ class IOTest < Test::Unit::TestCase
  end
 
  def test_readrows
-   filename=Dir[File.join(MYDIR,'io_test.txt')].first
+   filename=Dir[File.join(__dir__,'io_test.txt')].first
    expect=[["A1","B1","C1"],["A2","B2","C2"],["A3","B3","C3"]]
    actual=IO.readrows(filename,:row=>"=",:col=>"-")
    assert_equal(expect,actual)
