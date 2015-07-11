d0271a3a318509536cf6e592272f8a2b8519433f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-10 18:02:04 -0600

    Refactor Enumerable#each_at and add tests

--- lib/sixarm_ruby_ramp/enumerable/each.rb
+++ lib/sixarm_ruby_ramp/enumerable/each.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 module Enumerable
 
-  # Get each_element at a given index or indices.
+  # Get each element at a given index or indices.
   #
   # Example: use an index.
   #
@@ -29,10 +29,47 @@ module Enumerable
   #   => "e", "c", "d"
   #
   def each_at(filter)
-    # Normalize the filter to make it respond to #each and #include?.
+    filter, optimize_for_whole_numbers = each_at_normalize_filter(filter)
+
+    # Handle variations.
+    #
+    # Can we call self#at?
+    #
+    #  * Yes: look up a self element by index.
+    #  * No: iterate on self, comparing the loop count to filter target.
+    #
+    # Can we call filter#each?
+    #
+    #  * Yes: iterate on the filter elements.
+    #  * No:  iterate on self, comparing the loop count to filter#include?
+    #
+    # Can we optimize for whole numbers?
+    #
+    #  * Yes: we know that all filter targets are whole numbers.
+    #  * No: we must convert the filter target to an index size each time.
+    #
+    if self.respond_to?(:at) && filter.respond_to?(:each)
+      if optimize_for_whole_numbers
+        each_at_impl_with_self_at_and_filter_each_and_whole_numbers(filter)
+      else
+        each_at_impl_with_self_at_and_filter_each(filter)
+      end
+    elsif filter.respond_to?(:include?)
+      each_at_impl_with_worst_case(filter)
+    else
+      raise ArgumentError
+    end
+  end
+
+  protected
+
+  # Normalize the filter to make it respond to #each and #include?.
+  # If we can guarantee the filter is all whole numbers, then optimize.
+  #
+  def each_at_normalize_filter(filter)
     case filter
     when Numeric
-      filter = [i]
+      return [filter.to_i], filter >= 0
     when Range
       if filter.first < 0 || filter.last < 0
         min = filter.first.to_i; min += size if min < 0
@@ -40,28 +77,45 @@ module Enumerable
         max -= 1 if filter.exclude_end?
         filter = min..max
       end
-      optimize_for_whole_numbers = true
-    end
-    if respond_to?(:at) && filter.respond_to?(:each)
-      if optimize_for_whole_numbers
-        filter.each{|i|
-          yield(at(i))
-        }
-      else
-        filter.each{|i|
-          i += size if i < 0
-          yield(at(i))
-        }
-      end
-    elsif filter.respond_to?(:include?)
-      i = 0
-      each{|e|
-        yield(e) if filter.include?(i)
-        i += 1
-      }
+      return filter, true
     else
-      raise ArgumentError
+      return filter, false
     end
-
   end
+
+  # The #each_at method calls this method when we can use the
+  # best fast implementation, i.e. using self#at, filter#each,
+  # and knowing that all the filter elements are >= 0.
+  #
+  def each_at_impl_with_self_at_and_filter_each_and_whole_numbers(filter)
+    filter.each{|i|
+      yield(at(i))
+    }
+  end
+
+  # The #each_at method calls this method when we can use the
+  # second-best implementation, i.e. using self#at, filter#each,
+  # yet having to test each filter elements for negative indexing.
+  #
+  def each_at_impl_with_self_at_and_filter_each(filter)
+    filter.each{|i|
+      yield(at(i >= 0 ? i : i + self.size))
+    }
+  end
+
+  # The #each_at method calls this method when we cannot use self#at
+  # and filter#each, thus we must use a loop counter and iteration
+  # on the self elements, and test whether the counter is in the filter.
+  #
+  # This is the worst case solution, and is rarely needed in the wild.
+  #
+  def each_at_impl_with_worst_case(filter)
+    i = 0
+    s = respond_to?(:size) ? size : nil
+    each{|e|
+      yield(e) if (filter.include?(i) || (size ? filter.include(i - size)))
+      i += 1
+    }
+  end
+
 end
--- test/sixarm_ruby_ramp_test/enumerable/each_test.rb
+++ test/sixarm_ruby_ramp_test/enumerable/each_test.rb
@@ -7,9 +7,36 @@ require "sixarm_ruby_ramp/enumerable/each"
 
 class EnumerableEachTest < Minitest::Test
 
-  def test_map_id
-    x = [OpenStruct.new(id: 1), OpenStruct.new(id: 2), OpenStruct.new(id: 3)]
-    assert_equal(x.map(&:id), x.map_id)
+  def test_each_at_with_numeric_with_positive
+    assert_equal("a", ["a", "b", "c"].each_at(0))
+  end
+
+  def test_each_at_with_numeric_with_negative
+    assert_equal("c", ["a", "b", "c"].each_at(-1))
+  end
+
+  def test_each_at_with_range_with_positives
+    assert_equal(["a", "b"], ["a", "b", "c"].each_at(0..1))
+  end
+
+  def test_each_at_with_range_with_negatives
+    assert_equal(["c", "b"], ["a", "b", "c"].each_at(-1..-2))
+  end
+
+  def test_each_at_with_enumerable_ascending
+    assert_equal(["a", "c"], ["a", "b", "c"].each_at([0, 2]))
+  end
+
+  def test_each_at_with_enumerable_descending
+    assert_equal(["c", "a"], ["a", "b", "c"].each_at([2, 0]))
+  end
+
+  def test_each_at_with_worst_case_using_all_of_the_above_tests
+    assert_equal("a", ["a", "b", "c"].each_at(0))
+    assert_equal("c", ["a", "b", "c"].each_at(-1))
+    assert_equal(["a", "b"], ["a", "b", "c"].each_at(0..1))
+    assert_equal(["a", "c"], ["a", "b", "c"].each_at([0, 2]))
+    assert_equal(["c", "a"], ["a", "b", "c"].each_at([2, 0]))
   end
 
 end
